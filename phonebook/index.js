178de1353dfac8bbea147af03bb4468efc70f626
Kostas <kostasp@icloud.com>
2023-11-23 03:12:22 +0100

    3.6
    Part 3.6 has been implement

--- phonebook/index.js
+++ phonebook/index.js
@@ -33,7 +33,7 @@ app.get('/api/persons' , (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const howManyPersons = persons.length
+    const howManyPersons = Object.keys(persons).length
     const timestamp = 1637551200000
     const date = new Date(timestamp)
     response.send(`<p>Phonebook has info for ${howManyPersons} people</p><p>${date}</p>`)
@@ -42,13 +42,47 @@ app.get('/info', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
-    console.log(id)
-    console.log(person)
+    
     if(person){
         response.json(person)
     }else {
         response.status(404).end()
       } 
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end() 
+})
+
+const generatedId = () => {
+    const maxId = persons.reduce((max, person) => (person.id > max ? person.id : max), persons[0].id)
+    return Math.floor(Math.random() * (maxId + 200))
+} 
+
+app.post('/api/persons',(request, response) => {
+    const body = request.body
+
+    const person = {
+        id: generatedId(),
+        name: body.name,
+        number: body.number,
+    }
+    if(!body.name || !body.number){
+        return response.status(400).json({
+            error: "content is missing"
+          })
+    } else if(persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: "name must be unique"
+          })
+    } 
+    else {
+        persons = persons.concat(person)
+        response.json(person)
+    }
+    
 })
 
 const PORT = 3001
