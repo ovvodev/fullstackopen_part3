d03697db0cbbeabb1b45a18247ef6f36da945176
Kostas <kostasp@icloud.com>
2023-11-28 20:18:45 +0100

    fly
    implement fly

--- notes/index.js
+++ notes/index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-
-
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -19,71 +17,81 @@ const unknownEndpoint = (request, response) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-
+app.use(express.static('build'))
 
 let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-  
-app.get('/', (request, response) => {
-    response.send('<h1>hello world!</h1>')
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response)=> {
-    response.json(notes)
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if(note){
-    response.json(note)
-  }else {
-    response.status(404).end()
-  } 
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id === id)
-  response.status(204).end()
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
   return maxId + 1
 }
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
-  
-  if(!body.content){
-    return response.status(400).json({
-      error: "contnent is missing"
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
     })
   }
 
   const note = {
     content: body.content,
     important: body.important || false,
-    id: generateId()
+    date: new Date(),
+    id: generateId(),
   }
+
   notes = notes.concat(note)
+
   response.json(note)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
 })
 
 app.use(unknownEndpoint)
