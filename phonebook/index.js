0d179ab56a413d453b4da22554a7adfee0b86321
Kostas <kostasp@icloud.com>
2023-12-04 00:32:27 +0100

    testing deploy to fly 3.13

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
 const Person = require('./models/person')
+
 
 app.use(express.json())
 app.use(cors())
@@ -56,8 +59,10 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+    
+    const person = Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
     
     if(person){
         response.json(person)
@@ -72,19 +77,19 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end() 
 })
 
-const generatedId = () => {
+/*const generatedId = () => {
     const maxId = persons.reduce((max, person) => (person.id > max ? person.id : max), persons[0].id)
     return Math.floor(Math.random() * (maxId + 200))
-} 
+} */
 
 app.post('/api/persons',(request, response) => {
     const body = request.body
     
-    const person = {
-        id: generatedId(),
+    const person = new Person ({
+        
         name: body.name,
         number: body.number,
-    }
+    })
     if(!body.name || !body.number){
         return response.status(400).json({
             error: "content is missing"
@@ -95,15 +100,16 @@ app.post('/api/persons',(request, response) => {
           })
     } 
     else {
-        persons = persons.concat(person)
-        response.json(person)
+       person.save().then(savedPerson => {
+         response.json(savedPerson)
+       })  
     }
     
 })
 
 
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
