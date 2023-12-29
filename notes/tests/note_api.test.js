83d3643128d54834573aa79479bfbbc43928120d
Kostas <kostasp@icloud.com>
2023-12-29 20:29:02 +0100

    supertest

--- notes/tests/note_api.test.js
+++ notes/tests/note_api.test.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
-
 const api = supertest(app)
+
+const Note = require('../models/note')
+const initialNotes = [ 
+   {    content: 'HTML is easy',    important: false,  },  {    content: 'Browser can execute only JavaScript',    important: true,  },
+  ]
+
+beforeEach(async () => { 
+   await Note.deleteMany({})  
+   let noteObject = new Note(initialNotes[0])  
+   await noteObject.save()  
+   noteObject = new Note(initialNotes[1])  
+   await noteObject.save()
+  })
 
 test('notes are returned as json', async () => {
   await api
@@ -11,6 +22,20 @@ test('notes are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 },50000)
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body).toHaveLength(initialNotes.length)
+})
+
+test('a specific note is within the returned notes', async () => { 
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)  
+  expect(contents).toContain(  
+    'Browser can execute only JavaScript'  )
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
